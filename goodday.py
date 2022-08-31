12f5afa8ca7ca090b5f8cfc1e28341c48c54a303
Isaac Hoyos <gamemasterx40@gmail.com>
2022-08-31 14:05:44 -0400

    Add files via upload

---
+++ goodday.py
@@ -0,0 +1,12 @@
+# File: goodday.py
+# This is our first time in class doing scripts.
+
+# Our first function
+def goodday():
+    print("Good day")
+    print("How are you?")
+
+# Our second function
+def goodevening(name):
+    print("Good evening ", name)
+    print("Hey " + name + " How about coffee?")
