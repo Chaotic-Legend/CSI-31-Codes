0cfc0860cff3222d740f4359c841494d6ee9a5e9
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-07 11:12:22 -0400

    Add files via upload

---
+++ severalprint.py
@@ -0,0 +1,10 @@
+# File: severalprint.py
+# A program to illustrate several print statements.
+
+def main():
+    print("How about" , end = " ")
+    print("coffee?")
+    print("The sum of 3 and 5 is" , end = " ")
+    print(3 + 5)
+
+main()
