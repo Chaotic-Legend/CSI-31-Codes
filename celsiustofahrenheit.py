0c279d5de350d95e409e1a36f2870dba30fdbb72
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-07 10:51:45 -0400

    Add files via upload

---
+++ celsiustofahrenheit.py
@@ -0,0 +1,9 @@
+# File: celsiustofahrenheit.py
+# A program to convert Celsius temperature to Fahrenheit.
+
+def main():
+    celsius = eval(input("What is the Celsius temperature?"))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperautre is" , fahrenheit , "degrees Fahrenheit.")
+
+main ()
---
+++ fahrenheittocelsius.py
@@ -0,0 +1,9 @@
+# File: fahrenheittocelsius.py
+# A program to convert Celsius temperature to Fahrenheit.
+
+def main():
+    fahrenheit = eval(input("What is the Fahrenheit temperature?"))
+    celsius = 5 * (fahrenheit - 32) / 9
+    print("The temperautre is" , celsius , "degrees Celsius.")
+
+main()
