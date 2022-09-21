2df3b251cafd7f8bffbe155c82999d30bd62e431
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-21 11:41:55 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+# Our first Tkinter Program.
+# File Name: hello.py
+
+from tkinter import *
+
+class Window(Frame):
+    def init(self , master = None):
+        Frame.init(self , master)
+        
+        self.master = master
+
+root = Tk()
+
+
+#app = Window(root)
+w = Label(root , text = "Hello, world!")
+w.pack()
+
+root.mainloop()
