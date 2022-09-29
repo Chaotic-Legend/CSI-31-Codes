ac3acd24743ccb1acc957c14d8be661cd8fef320
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-29 19:19:24 -0400

    Update average_of_two_numbers_second.py

--- average_of_two_numbers_second.py
+++ average_of_two_numbers_second.py
@@ -1,11 +1,11 @@
 # File: average_of_two_numbers_second.py
-# A program to find the average of two numbers.
+# Another program to find the average of two numbers.
 
 def main():
     print("This program prints the average of two numbers.")
     s1 = eval(input("Enter your first score: "))
     s2 = eval(input("Enter your second score: "))
     average = (s1 + s2) / 2
-    print("The average of your scores is" , average)
+    print("The average of your scores is", average)
 
 main ()
