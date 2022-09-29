81a0934a94baa9babe9a2c3f42b522ac46990d17
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-29 18:23:31 -0400

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -3,13 +3,13 @@
 
 def main():
     print("This program illustrates a chaotic function.")
-x = eval(input("Enter a number between 0 and 1: " ))
-if x > 0 and x < 1:
-    for i in range(10):
-        x = 3.9 * x * (x-1)
-        print(x)
-
-else:
-    print("Your number x is out of range.")
+    x = eval(input("Enter a number between 0 and 1: "))
+    if x > 0 and x < 1:
+        for i in range(10):
+            x = 3.9 * x * (x - 1)
+            print(x)
+    
+    else:
+        print("Your number x is out of range.")
     
 main()
