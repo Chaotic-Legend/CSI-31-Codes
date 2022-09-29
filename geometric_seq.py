93f2f9c29f5fe82678be55919a3b62fcf292ee5d
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-29 19:25:29 -0400

    Update geometric_seq.py

--- geometric_seq.py
+++ geometric_seq.py
@@ -2,15 +2,14 @@
 # File: geometric_seq.py
 # A program to compute the 15-th term of geometric sequence.
 
-def main ():
+def main():
     print("This program calculates the fifteenth term of a geometric sequence.")
-    
     term = eval(input("Enter the starting term: "))
     ratio = eval(input("Enter the common ratio: "))
-    
     for i in range(15):
+        print(term)
         term = term * ratio
 
-    print("The fifteenth term is" , term)
+    print("The fifteenth term of this geometric sequence is", term)
 
-main ()
+main()
