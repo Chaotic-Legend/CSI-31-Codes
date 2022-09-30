846fef80544cb0667100153d24d687b387968802
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 19:59:25 -0400

    Update arithmetic_seq.py

--- arithmetic_seq.py
+++ arithmetic_seq.py
@@ -1,15 +1,15 @@
 # Here is the introduction.
 # File: arithmetic_seq.py
-# A program to compute the 15-th term of an arithmetic sequence.
+# This script computes the nth term of an arithmetic sequence.
 
 def main():
-    print("This program calculates the fifteenth term of an arithmetic sequence.")
-    term = eval(input("Enter the starting term: "))
-    difference = eval(input("Enter the common difference: "))
-    for i in range(15):
+    print("This program calculates the nth term of an arithmetic sequence.")
+    term = float(input("Enter the starting term: "))
+    difference = float(input("Enter the common difference: "))
+    nth = eval(input("Enter the nth term: "))
+    for i in range(nth):
         print(term)
         term = term + difference
-    
-    print("The fifteenth term of this arithmetic sequence is", term)
+    print("The", nth, "term of this arithmetic sequence is", term)
 
 main()
