8c00127dc131f6568452404832a2ccd926380677
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 19:30:31 -0400

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,24 +1,28 @@
 # Here is the introduction.
 # File: quadratic.py
-# A program that computes the real roots of a quadratic equation and illustrates the use of the math library.
-# Note: The program crashes if there is no real root for the equation.
+# This script computes the real roots of a quadratic equation, evaluates the quadratic equation, and also illustrates the use of the math library.
+# Note: This program will tell if there is no real root for the quadratic equation.
 
 import math # This module makes the math library available to use.
 
 def main():
-    print("This program finds the real roots to a quadratic equation.")
+    print("This script finds the real roots of a quadratic equation and evaluates the quadratic equation.")
     print()
-
-    a = float(input("Enter coefficient a: "))
-    b = float(input("Enter coefficient b: "))
-    c = float(input("Enter coefficient c: "))
-    
-    discRoot = math.sqrt(b * b - 4 * a * c)
-    root1 = (-b + discRoot) / (2 * a)
-    root2 = (-b - discRoot) / (2 * a)
-    
-    print()
-    
-    print("The two roots are" , root1 , root2)
+    a = float(input("Enter the coefficient for a: "))
+    b = float(input("Enter the coefficient for b: "))
+    c = float(input("Enter the coefficient for c: "))
+    x = float(input("Enter the value for x to evaluate: "))
+    value = (a * (x**2) + b * x + c)
+    disc = (b**2 - 4 * a * c)
+    if disc < 0:
+        print()
+        print("Sorry, your quadratic equation has no real roots.")
+    else:
+        discRoot = math.sqrt(disc)
+        root1 = (-b + discRoot) / (2 * a)
+        root2 = (-b - discRoot) / (2 * a)
+        print()
+        print("The two roots of this quadratic equation are", root1, "and", root2)
+        print("The evaluation at x =", x, "is", value)
 
 main()
