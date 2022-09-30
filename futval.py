7b36359217954f5c99d2eddbb2e535c155083e07
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 10:30:49 -0400

    Update futval.py

--- futval.py
+++ futval.py
@@ -1,16 +1,15 @@
 # Here is the introduction.
 # File: futval.py
-# A program to compute the value of an investment carried 10 years into the future.
+# A program to compute the value of an investment carried out in a specific amount of years into the future.
 
 def main():
-    print("This program calculates the future value of a 10 year investment.")
-
+    print("This program calculates the future value of an investment in a specific amount of years.")
     principal = eval(input("Enter the initial principal: "))
-    apr = eval(input("Enter the annual interest rate : "))
-    
-    for i in range(10):
+    apr = eval(input("Enter the annual interest rate: "))
+    years = eval(input("How many years should we look ahead? "))
+    for i in range(years):
         principal = principal * (1 + apr)
     
-    print("The value of your investment in 10 years is" , principal)
+    print("The value of your investment in", years, "years is", principal, "dollars.")
 
-main ()
+main()
