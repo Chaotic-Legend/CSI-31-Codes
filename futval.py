aa4f6bad5aef23495513365d0fc92a4fd8d29cbe
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 19:54:09 -0400

    Update futval.py

--- futval.py
+++ futval.py
@@ -1,6 +1,6 @@
 # Here is the introduction.
 # File: futval.py
-# A program to compute the value of an investment carried out in a specific amount of years into the future.
+# This script computes the value of an investment carried out in a specific amount of years into the future.
 
 def main():
     print("This program calculates the future value of an investment in a specific amount of years.")
@@ -9,7 +9,6 @@ def main():
     years = eval(input("How many years should we look ahead? "))
     for i in range(years):
         principal = principal * (1 + apr)
-    
     print("The value of your investment in", years, "years is", principal, "dollars.")
 
 main()
