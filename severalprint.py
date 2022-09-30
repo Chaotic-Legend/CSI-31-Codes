8c8b2c449bea62734dbc0e1687375b869baf8cd6
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 11:04:47 -0400

    Update severalprint.py

--- severalprint.py
+++ severalprint.py
@@ -2,9 +2,10 @@
 # A program to illustrate several print statements.
 
 def main():
-    print("How about" , end = " ")
-    print("coffee?")
-    print("The sum of 3 and 5 is" , end = " ")
+    print("How about", end = " ")
+    print("some coffee?")
+    print()
+    print("The sum of 3 and 5 is", end = " ")
     print(3 + 5)
     print("The sum of 2 and 6 is", 2 + 6)
 
