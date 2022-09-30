b0ea202ed27e56c27687a70aae5e2d7d97fe7a7b
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 11:11:28 -0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -6,12 +6,10 @@ def main():
     print("Correct Factorial")
     print()
     n = int(input("Enter a whole number: "))
-    
     fact = 1
-    for i in range(1 , n + 1):
+    for i in range(1, n + 1):
         fact = fact * i
-    
     print()
-    print("The factorial of " , n , " is " , fact)
+    print("The factorial of", n, "is", fact)
 
 main()
