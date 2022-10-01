63e9379ddb1aa67e25706ad0337afeac48a5b4b6
Isaac Hoyos <gamemasterx40@gmail.com>
2022-10-01 12:42:33 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,14 @@
-# Our first Tkinter Program.
+# Our first Tkinter program.
 # File Name: hello.py
 
 from tkinter import *
-
 class Window(Frame):
-    def init(self , master = None):
-        Frame.init(self , master)
-        
+    def init(self, master = None):
+        Frame.init(self, master)
         self.master = master
-
 root = Tk()
 
 #app = Window(root)
-w = Label(root , text = "Hello, World!")
+w = Label(root, text = "Hello, World!")
 w.pack()
-
 root.mainloop()
