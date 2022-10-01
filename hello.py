e411871dae18f1014386d8be75e4e3e64e19bbd5
Isaac Hoyos <gamemasterx40@gmail.com>
2022-10-01 12:44:55 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,8 +7,7 @@ class Window(Frame):
         Frame.init(self, master)
         self.master = master
 root = Tk()
-
-#app = Window(root)
+app = Window(root)
 w = Label(root, text = "Hello, World!")
 w.pack()
 root.mainloop()
