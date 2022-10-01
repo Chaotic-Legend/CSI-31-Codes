701ffb5bbfcf78998f90b26db2e44d1b4f4f058c
Isaac Hoyos <gamemasterx40@gmail.com>
2022-10-01 15:05:56 -0400

    Update arithmetic_seq.py

--- arithmetic_seq.py
+++ arithmetic_seq.py
@@ -7,7 +7,7 @@ def main():
     term = float(input("Enter the starting term: "))
     difference = float(input("Enter the common difference: "))
     nth = eval(input("Enter the nth term: "))
-    for i in range(nth):
+    for i in range(nth - 1):
         print(term)
         term = term + difference
     print("The", nth, "term of this arithmetic sequence is", term)
