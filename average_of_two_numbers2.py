e181f8050382746dad9cec60fc01cdea2898521b
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-30 20:13:14 -0400

    Update average_of_two_numbers2.py

--- average_of_two_numbers2.py
+++ average_of_two_numbers2.py
@@ -1,10 +1,10 @@
-# File: average_of_two_numbers_second.py
-# Another program to find the average of two numbers.
+# File: average_of_two_numbers2.py
+# Another script to find the average of two numbers.
 
 def main():
-    print("This program prints the average of two numbers.")
-    s1 = eval(input("Enter your first score: "))
-    s2 = eval(input("Enter your second score: "))
+    print("This program calculates the average of two numbers.")
+    s1 = float(input("Enter your first score: "))
+    s2 = float(input("Enter your second score: "))
     average = (s1 + s2) / 2
     print("The average of your scores is", average)
 
