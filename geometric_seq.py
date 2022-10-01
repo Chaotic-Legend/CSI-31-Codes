574879998ce1ade9543bfcbfea28b91be0444c91
Isaac Hoyos <gamemasterx40@gmail.com>
2022-10-01 15:10:50 -0400

    Update geometric_seq.py

--- geometric_seq.py
+++ geometric_seq.py
@@ -7,7 +7,7 @@ def main():
     term = float(input("Enter the starting term: "))
     ratio = float(input("Enter the common ratio: "))
     nth = eval(input("Enter the nth term: "))
-    for i in range(nth):
+    for i in range(nth - 1):
         print(term)
         term = term * ratio
     print("The", nth, "term of this geometric sequence is", term)
