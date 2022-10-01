6dbdb0471d96fcc653251219cfddceae308175ce
Isaac Hoyos <gamemasterx40@gmail.com>
2022-10-01 14:07:27 -0400

    Update change2.py

--- change2.py
+++ change2.py
@@ -1,18 +1,19 @@
 # Here is the introduction.
 # File: change2.py
-# A program to calculate the value of loose change.
+# A different script to count your loose change to calculate the total value in dollars.
 
 def main():
-    print("Change Counter")
+    print("This program is a loose change counter that gives the total value in dollars.")
     print()
-    print("Please enter the count of each coin type.")
+    print("Please enter the exact number of coins you have for each type of coin.")
+    dollarcoin = int(input("Dollar Coins: "))
+    halfdollar = int(input("Half Dollar Coins: "))
     quarters = int(input("Quarters: "))
     dimes = int(input("Dimes: "))
     nickels = int(input("Nickles: "))
     pennies = int(input("Pennies: "))
-    total = quarters * .25 + dimes * .10 + nickels * .05 + pennies * .01
+    total = dollarcoin * 1.00 + halfdollar * 0.50 + quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
     print()
-    
-    print("The total value of your change is", total)
+    print("The total value of your loose change in dollars is", total)
 
 main()
