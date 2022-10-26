f9d53634049c6a81cd022fcafa04f9f2dd0f40dd
Isaac Hoyos <gamemasterx40@gmail.com>
2022-10-26 11:12:36 -0400

    Update fahrenheit_to_celsius.py

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -2,7 +2,7 @@
 # This program converts Farenheit temperature to Celsius.
 
 def main():
-    fahrenheit = eval(input("What is the Fahrenheit temperature? "))
+    fahrenheit = float(input("What is the Fahrenheit temperature? "))
     celsius = (5 * (fahrenheit - 32) / 9)
     print("\nThe temperautre is", round(celsius, 4), "degrees Celsius.")
 
