f9995a117ff88c6c26ef647bc58aa01c08103d34
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-04 18:16:57 -0400

    Update change.py

--- change.py
+++ change.py
@@ -1,10 +1,9 @@
 # Here is the introduction.
 # File: change.py
-# This script counts loose change to give the total value in dollars.
+# This program counts loose change to give the total value in dollars.
 
 def main():
-    print("This program is a loose change counter that gives the total value in dollars.")
-    print()
+    print("This program is a loose change counter that gives the total value in dollars.\n")
     print("Please enter the exact number of coins you have for each type of coin.")
     dollarcoin = eval(input("Dollar Coins: "))
     halfdollar = eval(input("Half Dollar Coins: "))
@@ -13,7 +12,6 @@ def main():
     nickels = eval(input("Nickles: "))
     pennies = eval(input("Pennies: "))
     total = dollarcoin * 1.00 + halfdollar * 0.50 + quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
-    print()
-    print("The total value of your loose change in dollars is", round(total, 3))
+    print("\nThe total value of your loose change in dollars is ${0}.".format(round(total, 3)))
 
 main()
