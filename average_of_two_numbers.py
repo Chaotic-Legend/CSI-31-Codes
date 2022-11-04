e0b7d3c66195d6ed04e94e9411f6ddb559716267
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-04 18:54:43 -0400

    Update average_of_two_numbers.py

--- average_of_two_numbers.py
+++ average_of_two_numbers.py
@@ -1,22 +1,22 @@
 # File: average_of_two_numbers.py
-# This script will find the average of two numbers.
+# This program will find the average of two numbers.
 
 def auxiliary(x,y):
     return (x + y) / 2
 
 def main():
-    print("This program calculates the average of two numbers.")
+    print("This program calculates the average of two numbers.\n")
     s1, s2 = eval(input("Enter two scores with a comma in between them: "))
-    print("The average of your two scores is.", auxiliary(s1,s2))
+    print("\nThe average of your two scores is {0}".format(auxiliary(s1, s2)))
 
 main() 
 
 print()
 
-def main():
-    print("This program calculates the average of two numbers.")
+def average():
+    print("This second program calculates the average of two numbers.\n")
     s1, s2 = eval(input("Enter two scores with a comma in between them: "))
     average = (s1 + s2) / 2
-    print("The average of your two scores is.", average)
+    print("\nThe average of your two scores is {0}".format(average))
 
-main()
+average()
