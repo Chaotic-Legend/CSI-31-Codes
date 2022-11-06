03e800a4ea848e381c6b627f813f00d8e538ad55
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-06 10:50:05 -0500

    Update test1_question6.py

--- Test 1 Questions/test1_question6.py
+++ Test 1 Questions/test1_question6.py
@@ -1,11 +1,11 @@
 # Isaac D. Hoyos
 # Test 1: Question 6
 
-def main():
+def reverse():
     title = (input("Please enter a word or a number to be the string: "))
     L = list(title)
     L.reverse()
     backTitle = "".join(L)
     print("\nThe string in reverse order can be obtained by doing:", backTitle)
 
-main()
+reverse()
