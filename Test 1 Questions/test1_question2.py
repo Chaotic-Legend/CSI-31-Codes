dd34e0a3d21e0b90d7971e85b4f6b5c1c53dae04
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-06 10:35:46 -0500

    Update test1_question2.py

--- Test 1 Questions/test1_question2.py
+++ Test 1 Questions/test1_question2.py
@@ -2,9 +2,9 @@
 # Test 1: Question 2
 
 def main():
-    for x in range (-2, 3):
+    for x in range(-2, 3):
         for y in range(-2, 3):
             if x <= y:
-                print((x, y), end = " ")
+                print((x, y), end = "\n")
 
 main()
