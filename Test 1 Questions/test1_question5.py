835a504afede66de72b4253e8c8107063cb9cf35
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-06 10:48:05 -0500

    Update test1_question5.py

--- Test 1 Questions/test1_question5.py
+++ Test 1 Questions/test1_question5.py
@@ -1,11 +1,10 @@
 # Isaac D. Hoyos
 # Test 1: Question 5
 
-import math
-from math import *
 def main():
+    import math
     r = float(input("Please enter a numeric value for the radius of the circle: "))
-    P = 2 * math.pi * r
-    print("The perimeter of the circle is P =", P)
+    C = (2 * math.pi * r)
+    print("\nThe circumference of the circle is C ≈", round(C, 2))
 
 main()
