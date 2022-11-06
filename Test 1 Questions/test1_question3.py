5e0c6d3cbf97f1ccfcc89a9dad73575b00068016
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-06 10:37:42 -0500

    Update test1_question3.py

--- Test 1 Questions/test1_question3.py
+++ Test 1 Questions/test1_question3.py
@@ -1,8 +1,8 @@
 # Isaac D. Hoyos
 # Test 1: Question 3
 
-def main():
-    print("This program calculates the tenth term of an arithmetic sequence.")
+def arithmetic():
+    print("This program calculates the tenth term of an arithmetic sequence.\n")
     term = float(input("Enter the starting term: "))
     difference = float(input("Enter the common difference: "))
     for i in range(10 - 1):
@@ -10,4 +10,4 @@ def main():
         term = term + difference
     print("The tenth term of this arithmetic sequence is", term)
 
-main()
+arithmetic()
