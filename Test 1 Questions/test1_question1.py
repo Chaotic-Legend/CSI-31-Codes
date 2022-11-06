25744c42be051d9f9c43d6bf27bf833fbf22767f
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-06 10:34:33 -0500

    Update test1_question1.py

--- Test 1 Questions/test1_question1.py
+++ Test 1 Questions/test1_question1.py
@@ -2,10 +2,10 @@
 # Test 1: Question 1
 
 import math
-from math import *
 from math import log as ln
+
 def main():
-    print("When ∛2 - ln(π) + cos(e) =", pow(2, 1/3) - ln(pi) + math.cos(e))
-    print("The approximated rounded value of ∛2 - ln(π) + cos(e) is", round(pow(2, 1/3) - ln(pi) + math.cos(e), 3))
+    print("∛2 - ln(π) + cos(e) =", pow(2, 1/3) - ln(math.pi) + math.cos(math.e))
+    print("\nThe approximated rounded value of ∛2 - ln(π) + cos(e) is", round(pow(2, 1/3) - ln(math.pi) + math.cos(math.e), 3))
 
 main()
