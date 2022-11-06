c6fc1d5f556fc4744f288c897c211208c5f28af6
Isaac Hoyos <gamemasterx40@gmail.com>
2022-11-06 10:28:27 -0500

    Update review1_question5.py

--- Test 1 Questions/review1_question5.py
+++ Test 1 Questions/review1_question5.py
@@ -1,14 +1,13 @@
 # Isaac D. Hoyos
 # Review For Test 1: Question 5
 
-def main():
+def quadratic():
     a = float(input("Enter the value for a in the quadratic equation ax^2 + bx + c: "))
     b = float(input("Enter the value for b in the quadratic equation ax^2 + bx + c: "))
     c = float(input("Enter the value for c in the quadratic equation ax^2 + bx + c: "))
     x = float(input("Enter the value for x to evaluate: "))
-    print()
-    print("The function of your parabola is f(x) =", a, "x^2 +", b, "x +", c)
     f = (a * (x**2) + b * x + c)
-    print("The evaluation at x =", x, "is f(", x, ") =", f)
+    print("\nThe function of your parabola is f(x) =", a, "x^2 +", b, "x +", c)
+    print("\nThe evaluation at x =", x, "is f(x) =", f)
 
-main()
+quadratic()
